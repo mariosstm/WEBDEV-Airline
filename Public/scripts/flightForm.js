26b75bc72fee3b8691d2451f72ac6bdead0359f8
Jonnyolsen1 <johnmastro98@gmail.com>
2022-05-31 15:18:19 +0300

    oxi allh css

--- Public/scripts/flightForm.js
+++ Public/scripts/flightForm.js
@@ -1,5 +1,6 @@
 const oneWayBtn = document.querySelector("#oneway");
 const swapBtn = document.querySelector(".swapLocBtn");
+const psngrBtn = document.querySelector(".passengers-btn");
 
 oneWayBtn.addEventListener("click", function() {
   const returnDate = document.querySelector("#returnDate");
@@ -17,4 +18,10 @@ swapBtn.addEventListener("click", function() {
   
   document.querySelector("#departure").value = arrCity;
   document.querySelector("#arrival").value = dptCity;
+})
+
+psngrBtn.addEventListener("click", function() {
+  console.log('here')
+  const dropDown = document.querySelector(".psngr-dropdown");
+  dropDown.style.display="block";
 })
