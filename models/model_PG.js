08a519803056730043922904872918c62e2cb899
Jonnyolsen1 <johnmastro98@gmail.com>
2022-06-01 16:37:37 +0300

    local

--- models/model_PG.js
+++ models/model_PG.js
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
  const pool = new pg.Pool({
-     /*
+     
         user: process.env.PGUSER,
-//     host: process.env.PGHOST,
-//     database: process.env.PGDATABASE,
-//     password: process.env.PGPASSWORD,
-//     port: process.env.PORT
-     */
-     connectionString:process.env.URI,
-     ssl:{
-         rejectUnauthorized:false
-     }
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: process.env.PORT
+     
+    //  connectionString:process.env.URI,
+    //  ssl:{
+    //      rejectUnauthorized:false
+    //  }
  })
 
 async function connect() {
