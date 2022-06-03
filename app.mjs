61dc1d1d31d0ad88be4d0adb738050744de39ec6
Jonnyolsen1 <johnmastro98@gmail.com>
2022-06-03 19:08:29 +0300

    app error fix

--- app.mjs
+++ app.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import {engine} from 'express-handlebars';
 import path from 'path';
 
-import cookieParser from 'cookie-parser';
+//import cookieParser from 'cookie-parser';
 import passport from 'passport';
 import session from 'express-session';
 import routes from './routes/routes.js';
